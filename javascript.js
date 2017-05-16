d8bd8e73649713ff59efdb907df27826c1f08e01
Mike Carlucci <mike@mikecarlucci.org>
2017-05-16 14:34:52 -0400

    first pust
    added local html, js, css

---
+++ javascript.js
@@ -0,0 +1,345 @@
+var captions = [
+
+
+	'Vader: "How am I ever going to explain this."',
+	'...and, uhm, I would have voted for URoRRuR \' R\'R a third time if I could.',
+	'...Did you ever hear the tragedy of Darth Sidious the Jerkface?',
+	'...Look, I\'m all for live and let live, but this \'minute podcast\' thing is the dumbest idea I\'ve ever heard!',
+	'(Vader\'s Krayt Dragon ring tone goes off. Awkward glances between Leia and Vader as Native Tatooiner runs away) Vader: "He\'ll be back.\" Leia: (with a smile) \"And in greater numbers!\\"',
+	'[Peter & Ella\'s house, Halloween 2021] Daddy has to record with Alex for the Han Solo movie, then we can go trick-or-treating.',
+	'94...',
+	'A Jawa would have at least came with a free robot...',	
+	'A survivor. Impressive. Most impressive.',
+	'A TUSKER?! Not while you\'re living under MY roof!',
+	'After you froze my last boyfriend in carbonite I decided to look for someone hotter.',
+	'Also, I love the idea that Vader has portraits of Jar Jar and C3PO up on his wall.',
+	'Although he never voiced it aloud, sometimes he had a really hard time understanding Sheev\'s plan. At this point, he wondered, was Sheev just messing with him or what?',
+	'Although he was usually unflappable under pressure, sometimes when he was caught off guard, he didn\'t know what to do. \"Try spinning,\" he thought. \"That\'s a good trick.\"',
+	'And now, your highness, we will discuss the location of your Hidden Rebel Date',
+	'And one more thing dad, his blast points are extremely accurate!',
+	'And this is my father, Dartholemew SpaceInvader.',
+	'And you expect to support my daughter by selling tribal artwork by the Beggar\'s Canyon freeway?',
+	'And you know what else, Dad? We\'re going to name the baby... Ben!',
+	'And you, sir?',
+	'And you\'re right,dad, the sand *does* get everywhere, (wink).',
+	'Anything happens to my daughter, I have a lightsaber and a shovel. I doubt anyone would miss you.',
+	'Anything you do to my daughter, I will do to you.',
+	'Are you doing this just because I said I hate sand. You have rebelled for the last time, daughter.',
+	'As he locked eyes with her dad, Mike finally realized what Leia meant by \'dark side.\'',
+	'At least he doesn\'t herd nerfs.',
+	'Be careful Leia. They walk in single file to hide their numbers.',
+	'But Dad, he LOVES New Yorker cartoons. Maybe this one can explain why it\'s funny!',
+	'Can we keep him? Can we?',
+	'Christ, what an asshole.',
+	'Dad Dad Dad, you have to hear this guy try and talk. Go on tell my Dad your name.',
+	'Dad, breathe. Just... breathe.',
+	'Dad, I know you don\'t allow \"sand people\" in the house, but I\'m dating one now.',
+	'Dad, I want you to meet Urrurr Urr Urrrrr. He\'s getting his degree in sand engineering!',
+	'Dad, is it oversharing if I told you he is wizard with that Gaderffii stick?',
+	'Dad, meet my date!\" \"You know I hate sand...I Mean......Native Tatooiners!',
+	'Dad, this is Arg!! Yowr!!!! Rakak!!! Quaraka!!!!! Jr.',
+	'Dad, this is URoRRuR\'R\'R. URoRRuR\'R\'R, this is my Dad.',
+	'Dad, this THING was at the door claiming you\'re HIS brother...',
+	'Dad, we\'re pregnant!',
+	'Dad, you missed one.',
+	'Dad! Meet my date!\" \"You know I hate sand n....native Tatooiners\"',
+	'Dad. Sand people did not egg our house last Halloween, although we are meant to think they did.',
+	'Dad... I want you to meet your brother......',
+	'Dad...don\'t freak out. He\'s easily startled.',
+	'Darchie Vunker: \"Besmooch the family name in the eyes of the community!\"',
+	'Darth had hoped that this rebel thing was just a phase but this time his daughter had taken it too far',
+	'Dator? I hardly knew her!',
+	'Did you ever hear the tragedy of URoRRuR\'R\'R the boyfriend?',
+	'Disney\'s remake of \"Guess Who\'s Coming to Dinner.\"',
+	'Don\'t be too proud of this sociological terror you\'ve constructed.',
+	'Don\'t worry Dad, it\'s just my brother in a costume. I wouldn\'t date a real s*%# person',
+	'Don\'t worry, Dad. He didn\'t bring any women or children.',
+	'Eechuta!',
+	'Father?',
+	'For some reason, that was a slow burn for me. But I get it now. I\'m \"voting\" for this one.',
+	'Funny? I thought I killed your family.',
+	'FYI, Her previous boyfriend was her brother.',
+	'Get Out, part 2',
+	'Get out.',
+	'Ghorf Who\'s Coming to Dinner',
+	'Ghorfas Coming to Dinner',
+	'ghorfas, dad, GHORFAS!',
+	'Guess I\'ll lay out my paper for this animal',
+	'Guess who\'s coming to dinner?',
+	'Have her home by 9, or you\'ll find out what I did to your relatives.',
+	'Have our sex games grown too elaborate?',
+	'He better not have brought any sand...',
+	'He hates sand too, Dad!',
+	'He knew in that moment, even as he was about to achieve the objective, that it was unlikely he\'d make it back to the future in time to save the past. The cycle would continue indefinitely, and he\'d be caught in this loop forever. How many times had he had this same thought in this same place? Hundreds? Millions? Billions? An eternity. He was trapped in time... just as his heart was trapped in love.',
+	'He owns his own Bantha dealership!',
+	'He said he knew grandma.',
+	'He tried to work up the courage to ask for her father\'s blessing, but he choked.',
+	'He\'s a great conversationalist.',
+	'He\'s actually really nice but has a strange fear of rat tails.',
+	'He\'s as clumsy as he is stupid!',
+	'He\'s coarse and rough and he gets *everywhere*',
+	'He\'s getting sand all over my carpet.',
+	'He\'s like an animal!!!',
+	'He\'s more precise then a stormtrooper',
+	'He\'s one of the good ones.',
+	'He\'s part of the Tusken Alliance and a Raider! Take him away!',
+	'He\'s selling \"Grit!',
+	'Hey dad, remember when you promised to teach me that nifty choking trick you do?',
+	'Hi, I\'d like to add you to my professional network on LinkedIn.',
+	'I call my own shots, largely based on an accumulation of data, and everyone knows it.',
+	'I didn\'t think we had any chemistry, but then he fell off his bantha and pretended to die...',
+	'I don\'t care what Universe you\'re from, that\'s gotta hurt!',
+	'I find your lack of taste disturbing.',
+	'I have a big announcement.',
+	'I hope you can \"sand\" off the rough edges, otherwise I \"Tus-can\" find where you live.',
+	'I just clicked on his singles\' profile and found his number.',
+	'I just know you\'ll love him, Dad. He hosts a podcast!',
+	'I just wanted to say, it\'s an honor to be here, and I\'m having a wonderful time.',
+	'I know, I know... you have a bad feeling about this. But hear me out.',
+	'I love sand people...not just the men, but the women and children too!',
+	'I love the framed photo of Jar Jar!!!! I also love that Facebook automatically capitalized Jar Jar lol',
+	'I should have killed you both when I had the chance.',
+	'I think my Dad knew your Mum.',
+	'I think your Dad slaughtered my family.',
+	'I told you, I hate sand people! They\'re coarse, rough, and they get EVERYWHERE!...Even in my daughter!',
+	'I tried out that new blind-dating service \'The Singles File\'. I\'m not sure how it works, but he assures me it\'s all in the numbers.',
+	'I was just reading about crimes committed with stolen lightsabers. I\'ve lost several myself.',
+	'I was raised in an orphanage after my village was slaughtered by a crazy guy with a lightsaber. - Sad, I know, but that\'s all in the past.',
+	'I will make it legal.',
+	'I wish I was taller.',
+	'I would have voted for URoRRuR\'R\'R a third time if I could.',
+	'I\'M PREGNANT! WITH TWINS! We\'re naming the girl Rey and the boy Snoke',
+	'If you scare this boyfriend off, he\'ll be back, and in greater numbers!',
+	'If you\'re not back by 10 o\'clock, I\'m destroying your home planet young lady.',
+	'Impressive... Least impressive.',
+	'Investigation is required for this sand date',
+	'It is I, C-3PO. You probably don\'t recognise me because of my Gaderffii stick.',
+	'It never occurred to me that I would fall in love with a Ghorfas, but I have, and nothing\'s going to change that.',
+	'It\'s actually a speed date session, they\'re just in single-file.',
+	'It\'s okay, Dad, he\'s a cartoonist.',
+	'Lee Dator? I don\'t even know her!',
+	'Leia I told you no more animals in the house',
+	'Leia, only you could be so bold!',
+	'Like · May 9 at 9:39am',
+	'Look, I\'m just not comfortable with my daughter dating a, oh what\'s the polite terminology, Person odd Sand? I\'m to understand they get EVERYWHERE, if you take my meaning.',
+	'Luke, if she does that to you again, you have my permission to kill her.',
+	'Mr. Vader\" or can I call you \"Ani\"?',
+	'My name is Ur\'ur\'urru, you killed my father, prepare to die!',
+	'Niall Eccles To his dismay, his daughter\'s latest love interest was just her type: half-witted, scruffy-looking.',
+	'No, I never worked on a spice freighter! Why do people keep saying that?!',
+	'Nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo',
+	'Not again \"your highness.\" You weren\'t on any mercy mission this time.',
+	'Not the way he tells it...',
+	'Now this is speed dating!',
+	'Oh jeez, this guy again.',
+	'Oh yeah... I think he knew your grandmother.',
+	'Oh, hello Steve. How are your paren...oh, sorry.',
+	'Oh...my god. Lemme think.',
+	'OK, you can go to that Treidum kid\'s party... but it better not be another kegger!',
+	'Okay, right now, admittedly, he regrets freezing the last one.',
+	'Peter Jesshop I know Jedi call themselves masters and apprentices but it\'s not an s&m thing',
+	'Sand people\" is derogatory, dad.',
+	'Says he knew Grandma, and brings love from Shmi to you.',
+	'See this! THIS...is my GAFFIR STICK!!!\"',
+	'She better be home by twin-Sunset.',
+	'Sigh... this planet doesn\'t have a law against miscegenation, but I sure wish it had one against mis-sand-genation.',
+	'So, you have a twin sister...',
+	'Sorry, that\'s actually your brother too.',
+	'Strange as it was, their romance was still more believable than Anakin and Padme',
+	'Suffice it to say, grandma wouldn\'t have called him \'sandpeople,\" dad.',
+	'Thank God you\'re not dating a Wookie, that would kill your mother.',
+	'The closer I get to your daughter, the worse it gets. The thought of not being with her - I can\'t breathe. I\'m haunted by the kiss that she should never have given me. My heart is beating... hoping that kiss will not become a scar. She is in my very soul, tormenting me... what can I do? I will do anything she asks.',
+	'The only worse thing you could bring home is Sebulba!',
+	'They like to be called \"native tattooiners\" dad!',
+	'This is my dad. He\'s a Sith Lord, but don\'t let that intimidate you - he loves going to the podraces just as much as you do.',
+	'This is not podracing...',
+	'This is RruuRr, not RurrruuuR! Do you think all Native Tatooineers look alike, Dad?',
+	'This is where the fun... ends.',
+	'This nice young man says you might know where his women are? And the children too?',
+	'This will be a day long remembered. It has seen the end of this crossword puzzle, it will soon see the end of you living here.',
+	'Traitor!!',
+	'Tusken Raider... Darth Vader. Darth Vader... Tusken Raider.',
+	'Tusken: \"Have we met before?',
+	'Uh, Dad, this guy says his name is Ur\'oRRuRR\'oRuRRoRnigo Montoya and you killed his father?',
+	'Under the Tusken Twin Suns',
+	'Upon seeing a headshot of Jar-Jar Binks hanging on the wall, the Ghorfa suddenly realized his mistake.',
+	'Upset? No, no that was a long time ago in another life. You\'ll have to try harder then that Princess.',
+	'URoRRuR\'R\'R, dad. Dad, URoRRuR\'R\'R.',
+	'Vadar: O\'Reilly?',
+	'Vader goes for the modernist look. Who knew?',
+	'Vader: \"You a fan of podracing?\"',
+	'Wait, hold on. Did you just say... pod racing\'s not a \'real\' sport?',
+	'We would\'ve also accepted \"ballsballsballsballs.',
+	'We\'ll be fine at the landspeeder swap meet. He\'s only afraid of Krayt Dragons.',
+	'We\'re going to the beach. Want to come?',
+	'We\'re gonna try spinning, that\'s a neat trick!',
+	'We\'re just going to a party at Dan\'s house....Dan Tooine.',
+	'Well at least it\'s not your brother!',
+	'Well he certainly won\'t choke on his aspirations. Sounds like he\'s choking already',
+	'Well, this couldn\'t be any more awkward than that Cloud City dinner with your LAST boyfriend.',
+	'Well... poodoo.',
+	'What a misunderstanding!',
+	'What a relief! I thought you said \'Dator\'!',
+	'What are your aspirations for your evening out with my daughter?',
+	'What ever happened to that guy I froze?',
+	'What happened to your face Leia? You look strangely shiny.',
+	'What is it that you do? A Rater?',
+	'What the f*** are you talking about? The sandperson is not the issue here, dad. I\'m talking about drawing a line in the sand, dad. Across this line, you DO NOT... Also, dad, sandperson is not the preferred nomenclature. Native-Tattooiner, please.',
+	'What, is he proposing?',
+	'What!?!? I thought you said you were dating a Tuscan trader?',
+	'What?!',
+	'What?!!',
+	'When I speak do you hear David Prowse or James Earl Jones?',
+	'Who is this? Someone who loves you very much. [removes wrappings] It\'s your mother, Ani. Noooooooooooooooooo!',
+	'Why buy the Bantha when you can get the milk for free?',
+	'Why do you have to be so rebellious Leia?',
+	'Why would you bring that stick to a lightsaber duel?',
+	'Why yes, I am familiar with your home village.',
+	'Wow! You two know each other?!?',
+	'Yhe tension broke when it was revealed Glenn played for the Tatooine Wamp Rats, too.',
+	'You are to have her home by 9:30, pray I do not alter the deal further.',
+	'You have a lovely home. Did you use sand in your foundations?',
+	'You have done that yourself!',
+	'You know what this economy is like for grad students!',
+	'You may be \'her father\' but tonight she\'s calling me daddy.',
+	'You missed one!',
+	'You said I should date someone who understands where you come from, right?',
+	'You will pay for the damage to my podracer... with interest!',
+	'You\'d be dead!',
+	'You\'re crazy! Nothing Lennon or McCartney did after the Beatles was anything like as good as what George did!' 
+]
+
+
+	function newCaption() {
+		var randomNumber = Math.floor(Math.random() * (captions.length));
+		document.getElementById('captionDisplay').innerHTML = captions[randomNumber];
+	}
+
+
+	/* potential array to feed the captions instead
+	var quoteArray = [
+    {
+        content: "Graphic design is building systems",
+        author: "Michael Stinson"
+    },
+    {
+        content: "I rarely agree with what clients ask me to do.",
+        author: "Ross Lovegrove"
+    },
+    {
+        content: "I think most programmers spend the first 5 years of their career mastering complexity, and the rest of their lives learning simplicity.",
+        author: "Buzz Andersen"
+    },
+    {
+        content: "Make it simple. Make it memorable. Make it inviting to look at. Make it fun to read.",
+        author: "Leo Burnett"
+    },
+    {
+        content: "What makes great design great is not a trendy technique, but the logic and conceptual aspect that were figured out in the designer's mind &ndash; or on more likely, on paper &ndash; before a mouse cursor ever opened Photoshop.",
+        author: "Kyle Meyer"
+    },
+    {
+        content: "Designers tend to whisper, ad agencies tend to shout.",
+        author: " David Stuart"
+    },
+    {
+        content: "Design is the method of putting form and content together. Design, just as art, has multiple definitions; there is no single definition. Design can be art. Design can be aesthetics. Design is so simple, that's why it is so complicated.",
+        author: "Paul Rand"
+    },
+    {
+        content: "Tell it like it is, in a way they want to hear it.",
+        author: "Wihan Meerholz"
+    },
+    {
+        content: "The best way to find any and all errors is to publish your work.",
+        author: "Dan Vore"
+    },
+    {
+        content: "Creativity is piercing the mundane to find the marvelous.",
+        author: "Bill Moyers"
+    },
+    {
+        content: "I warn you against believing that advertising is a science.",
+        author: "Bill Bernbach"
+    }
+];
+
+//grab html elements
+var button = document.getElementById('quote-button'),
+    quote = document.getElementById('quote'),
+    author = document.getElementById('quote-author'),
+    tweet = document.getElementById('tweet'),
+    random;
+
+//Generate a Random Quote
+window.onload = randomQuote;
+button.addEventListener('click', randomQuote);
+
+//run the function so there are no double buttons happening
+tweetQuote();
+
+//random quote function
+function randomQuote (){
+    //get a random number to pick a random quote object
+    random = Math.floor(Math.random() * quoteArray.length);
+    //get that random quote's content
+    quote.innerHTML = quoteArray[random].content;
+    //get that random quote's author
+    author.innerHTML = '&mdash; ' + quoteArray[random].author;
+
+    //Tweet that new generated quote
+    tweetQuote();
+}
+
+//Dynamically Generate Content + Button
+function tweetQuote (){
+    var quoteShort = quote.innerHTML,
+        quoteShortAuthor = author.innerHTML.substr(2).trim(),
+        msg ='';
+
+    //are there two tweet buttons? also from http://jsfiddle.net/LEBz8/1/
+    var elem = document.getElementById('twitterbutton');
+    if (elem !== null) {
+        elem.parentNode.removeChild(elem);
+    }
+
+    //cut the message to fit the 140 length + leave some extra
+    if(quoteShort.length + quoteShortAuthor.length <= 90){
+        msg = '"' +quoteShort + '" by ' + quoteShortAuthor;
+    }else {
+        msg = '"'+ quoteShort.substr(0, 65) + '[...]" by ' + quoteShortAuthor;
+    }
+
+    //select the button using its class reference http://bit.ly/221t0Hw
+    var tweetDiv = document.querySelector('.twitter-share-button');
+
+    //make a new twitter button to dynamically generate stuff http://jsfiddle.net/LEBz8/1/
+    var link = document.createElement('a');
+
+    //set attributes for new button
+    link.setAttribute('href', 'https://twitter.com/share');
+    link.setAttribute('class', 'twitter-share-button');
+    link.setAttribute('id', 'tweet');
+    link.setAttribute('data-text', msg);
+    link.setAttribute('data-via', 'littleblacksmth');
+    link.setAttribute('data-size', 'large');
+
+    //replace the button for this one
+    tweetDiv.parentNode.replaceChild(link, tweetDiv);
+
+    //load twitter -- important
+    twttr.widgets.load();
+}
+
+! function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0],
+        p = /^http:/.test(d.location) ? 'http' : 'https';
+    if (!d.getElementById(id)) {
+        js = d.createElement(s);
+        js.id = id;
+        js.src = p + '://platform.twitter.com/widgets.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }
+}(document, 'script', 'twitter-wjs');
+
+*/
